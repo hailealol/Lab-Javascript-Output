58f88e66f267862b6a04240dee4aeb102c1021a2
hailealol <70154169+hailealol@users.noreply.github.com>
2020-12-09 13:03:42 -0500

    Add files via upload

---
+++ Javascript-Lab-Starter-Files-master/JS Output Challenge/script.js
@@ -0,0 +1,34 @@
+// Enter your code below to edit index.html
+
+yourName = "Hailea Reynolds"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "Software Development and Engineering Student | 16 Years Old"
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeChoice = "Allegheny College"
+document.getElementById("collegeChoice").innerHTML = collegeChoice;
+
+programChoice = "Computer Science"
+document.getElementById("programChoice").innerHTML = programChoice;
+
+collegeReason = "I chose Allegheny as it was the first college to actually come to mind and one to express interest in having me as a student. I'm honestly undecided on a college right now but I know I will most likely be doing Computer Science. I picked Computer Science as the program because it is a passion I have that I will realistically be pursuing."
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerChoice = "Software Engineer"
+document.getElementById("careerChoice").innerHTML = careerChoice;
+
+companyChoice = "Undecided"
+document.getElementById("companyChoice").innerHTML = companyChoice;
+
+careerReason = "I chose software engineering becuase it has been what I have wanted to pursue for a few years now."
+document.getElementById("careerReason").innerHTML = careerReason;
+
+q1 = "Treats the document as a tree structure where each object represents a part of the document."
+document.getElementById("q1").innerHTML = q1;
+
+q2 = "Inner HTML, document.write(), alert box, and console logs."
+document.getElementById("q2").innerHTML = q2;
+
+q3 = "JavaScript is able to manipulate all elements of an HTML page."
+document.getElementById("q3").innerHTML = q3;
